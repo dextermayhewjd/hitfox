c9130078155eb96d520dce5b8362f934390411b1
hailey liney <hh20889@bristol.ac.uk>
2023-02-06 15:13:57 +0000

    fixed camera movement (thanks jakub)

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,63 +1,89 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 7.5f;
-    public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
-    public Camera playerCamera;
-    public float lookSpeed = 2.0f;
-    public float lookXLimit = 45.0f;
+    // define the speed of an object
+    public float walkingSpeed;
+    public float speed;
+    public float rotationSpeed;
+    public float jumpSpeed;
 
-    public CharacterController characterController;
-    Vector3 moveDirection = Vector3.zero;
-    Vector2 rotation = Vector2.zero;
+    public bool sprinting = false;
+    public bool hidden;
 
-    [HideInInspector]
-    public bool canMove = true;
+    [SerializeField]
+    private Transform cameraTransform;
 
+    private float verticalSpeed;
+    private float stepOffset;
+    private CharacterController characterController;
+
+    // Start is called before the first frame update
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
-        rotation.y = transform.eulerAngles.y;
-    }
 
+        characterController = GetComponent<CharacterController>();
+        stepOffset = characterController.stepOffset;
+
+    }
+    
+    // Update is called once per frame
     void Update()
     {
-        if (characterController.isGrounded)
-        {
-            // We are grounded, so recalculate move direction based on axes
-            Vector3 forward = transform.TransformDirection(Vector3.forward);
-            Vector3 right = transform.TransformDirection(Vector3.right);
-            float curSpeedX = speed * Input.GetAxis("Vertical");
-            float curSpeedY = speed * Input.GetAxis("Horizontal");
-            moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
-            if (Input.GetButton("Jump"))
-            {
-                moveDirection.y = jumpSpeed;
-            }
+        // sprint
+        if (characterController.isGrounded) {
+            speed = Input.GetKey(KeyCode.LeftShift) ? walkingSpeed * 1.5f : walkingSpeed;  
         }
 
-        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-        // as an acceleration (ms^-2)
-        moveDirection.y -= gravity * Time.deltaTime;
+        // moving towards left or right using key A and D
+        float horizontalInput = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        
+        // moving forwards or backwards using key W and S
+        float verticalInput = Input.GetAxis("Vertical") * speed * Time.deltaTime; 
 
-        // Move the controller
-        characterController.Move(moveDirection * Time.deltaTime);
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        float magnitude = Mathf.Clamp01(movementDirection.magnitude) * speed;
+        movementDirection = Quaternion.AngleAxis(cameraTransform.rotation.eulerAngles.y, Vector3.up) * movementDirection;
+        movementDirection.Normalize();
 
-        // Player and Camera rotation
-        if (canMove)
-        {
-            rotation.y += Input.GetAxis("Mouse X") * lookSpeed;
-            rotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
-            rotation.x = Mathf.Clamp(rotation.x, -lookXLimit, lookXLimit);
-            playerCamera.transform.localRotation = Quaternion.Euler(rotation.x, 0, 0);
-            transform.eulerAngles = new Vector2(0, rotation.y);
+        verticalSpeed += Physics.gravity.y * Time.deltaTime;
+
+        if (characterController.isGrounded) {
+
+            verticalSpeed = -0.5f;
+            characterController.stepOffset = stepOffset;
+
+            if (Input.GetButtonDown("Jump")) {
+                verticalSpeed = jumpSpeed;
+            }
+        } else {
+            characterController.stepOffset = 0.0f;
+        }
+
+        // transform.Translate(movementDirection * speed * magnitude * Time.deltaTime, Space.World);
+
+        Vector3 velocity = movementDirection * magnitude;
+        velocity.y = verticalSpeed;
+        characterController.Move(velocity * Time.deltaTime);
+
+        if (movementDirection != Vector3.zero) {
+
+            Quaternion rotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+
         }
     }
-}
 
+    private void OnApplicationFocus(bool focusStatus) {
+        if (focusStatus) {
+            Cursor.lockState = CursorLockMode.Locked;
+        } else {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        
+    }
+}
