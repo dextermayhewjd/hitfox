43f489cbe1eb3aecb33a79e62ce46c613d44ade7
Jakub <jakubnavro25@gmail.com>
2023-02-06 13:26:00 +0000

    simple movement, camera control, and interaction

---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+ 
+public class PlayerInteraction : MonoBehaviour {
+ 
+    public Transform player;
+    public float interactionDistance;
+    bool active = false;
+ 
+    private void Update() {
+        InteractionRay();
+    }
+ 
+    void InteractionRay() {
+        RaycastHit hit;
+        active = Physics.Raycast(player.position, player.TransformDirection(Vector3.forward), out hit, interactionDistance);
+ 
+        if (active) {
+            IInteractable interactable = hit.collider.GetComponent<IInteractable>();
+ 
+            if (interactable != null) { 
+                if (Input.GetKeyDown(KeyCode.E)) {
+                    interactable.Interact();
+                }
+            }
+        }
+    }
+}
