d1824dd0322a4014d3499d0369563a7f89762b81
Jakub Navratil <jakubnavro25@gmail.com>
2023-03-15 03:25:17 +0000

    Bush hiding synced

--- Assets/Script/BushScript.cs
+++ Assets/Script/BushScript.cs
@@ -7,58 +7,51 @@ using Photon.Pun;
 public class BushScript : Interact
 {
     public bool inUse = false;
-    public Collider playerInBush = null;
+    public PhotonView playerInBush = null;
 
     // Update is called once per frame
     void Update()
     {
         if (colliders.Count != 0 && Input.GetButtonDown("Interact"))
         {
-            Debug.Log("Player interacted");
-
-            if (inUse && playerInBush.GetComponent<PhotonView>().IsMine)
+            if (inUse && playerInBush.IsMine)
             {
-                this.photonView.RPC("RPC_UnhidePlayer", RpcTarget.All);
-                // if (playerInBush.GetComponent<PhotonView>.IsMine) 
-                // {
-                    
-                // }
+                this.photonView.RPC("RPC_UnhidePlayer", RpcTarget.All, playerInBush.ViewID);
+                CinemachineFreeLook cam = FindObjectOfType<CinemachineFreeLook>();
+                cam.LookAt = playerInBush.transform;
+                cam.Follow = playerInBush.transform;
+                playerInBush = null;
             }
 
             else if (!inUse) 
             {
                 Debug.Log("If not in use");
-                this.photonView.RPC("RPC_HidePlayer", RpcTarget.All);
-                
-                // if (playerInBush.GetComponent<PhotonView>.IsMine) 
-                // {
-                    
-                // }
+                playerInBush = colliders.Find(x => x.GetComponent<PhotonView>().IsMine).GetComponent<PhotonView>();
+                if (playerInBush.IsMine) {
+                    CinemachineFreeLook cam = FindObjectOfType<CinemachineFreeLook>();
+                    cam.LookAt = transform;
+                    cam.Follow = transform;
+                }
+                this.photonView.RPC("RPC_HidePlayer", RpcTarget.All, playerInBush.ViewID);
             }
         }
     }
 
     [PunRPC]
-    void RPC_UnhidePlayer()
+    void RPC_UnhidePlayer(int disablePlayer)
     {
         inUse = !inUse;
-        playerInBush.transform.SetParent(null);
-        playerInBush.gameObject.SetActive(true);
-        CinemachineFreeLook cam = FindObjectOfType<CinemachineFreeLook>();
-        cam.LookAt = playerInBush.transform;
-        cam.Follow = playerInBush.transform;
-        playerInBush = null;
+        PhotonView dp = PhotonView.Find(disablePlayer);
+        dp.transform.SetParent(null);
+        dp.gameObject.SetActive(true);
     }
 
     [PunRPC]
-    void RPC_HidePlayer()
+    void RPC_HidePlayer(int disablePlayer)
     {
         inUse = !inUse;
-        playerInBush = colliders.Find(x => x.GetComponent<PhotonView>().IsMine);
-        playerInBush.transform.SetParent(this.transform);
-        playerInBush.gameObject.SetActive(false);
-        CinemachineFreeLook cam = FindObjectOfType<CinemachineFreeLook>();
-        cam.LookAt = transform;
-        cam.Follow = transform;
+        PhotonView dp = PhotonView.Find(disablePlayer);
+        dp.transform.SetParent(this.transform);
+        dp.gameObject.SetActive(false);
     }
 }
