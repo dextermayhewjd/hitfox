7371e9eb636b0fe25269b5611cf309ee9c8e3d8a
hailey liney <hh20889@bristol.ac.uk>
2023-04-05 16:37:48 +0100

    working but uname is placeholder and chat buttons don't work

--- Assets/Script/ChatBubbleDisplay.cs
+++ Assets/Script/ChatBubbleDisplay.cs
@@ -6,19 +6,28 @@ using TMPro;
 
 public class ChatBubbleDisplay : MonoBehaviour
 {
-    [SerializeField] TMP_Text text;
+    [SerializeField] PhotonView playerPV;
+    [SerializeField] private TMP_Text text;
 
 
     void Start() {
+        text = GetComponentsInChildren<TMP_Text>()[0]; // IF YOU REMOVE THE USELESS UsName AND ChatBubble GAMEOBJECTS FROM FOXPLAYER THEN CHANGE THIS
+        playerPV = GetComponent<PhotonView>();
+        text.text = "chat";
     }
 
     void Update() {
+
     }
 
     public IEnumerator ChangeTextForSeconds(string newtext, int secs) {
+        Debug.Log("ctfs");
+        Debug.Log(newtext);
+        Debug.Log("^newtext");
+        TMP_Text oldtext = text;
         text.text = newtext;
         yield return new WaitForSeconds(secs);
-        text.text = "";
+        text = oldtext;
     }
 
 }
