e99300a694b329f7fcaa9db456e223ee542205fc
dextermayhewjd <78476926+dextermayhewjd@users.noreply.github.com>
2023-04-10 18:18:48 +0100

    function is completed
    however the return to old text is done in Player chat instead of the ChatBubbleDisplay
    since you can see in the console
    but it works and function good right now

--- Assets/Script/ChatBubbleDisplay.cs
+++ Assets/Script/ChatBubbleDisplay.cs
@@ -7,12 +7,13 @@ using TMPro;
 public class ChatBubbleDisplay : MonoBehaviour
 {
     [SerializeField] PhotonView playerPV;
-    [SerializeField] private TMP_Text text;
-
+    // [SerializeField] private TMP_Text text;
+    [SerializeField] TMP_Text text;
+    public bool IsChangingText;
 
     void Start() {
-        text = GetComponentsInChildren<TMP_Text>()[0]; // IF YOU REMOVE THE USELESS UsName AND ChatBubble GAMEOBJECTS FROM FOXPLAYER THEN CHANGE THIS
-        playerPV = GetComponent<PhotonView>();
+        // text = GetComponentsInChildren<TMP_Text>()[0]; // IF YOU REMOVE THE USELESS UsName AND ChatBubble GAMEOBJECTS FROM FOXPLAYER THEN CHANGE THIS
+        // playerPV = GetComponent<PhotonView>();
         text.text = "chat";
     }
 
@@ -20,14 +21,26 @@ public class ChatBubbleDisplay : MonoBehaviour
 
     }
 
-    public IEnumerator ChangeTextForSeconds(string newtext, int secs) {
-        Debug.Log("ctfs");
-        Debug.Log(newtext);
-        Debug.Log("^newtext");
-        TMP_Text oldtext = text;
+    public string GetText(){
+        return text.text;
+    }
+
+    public void SetText(string newText){
+        text.text = newText;
+    }
+
+    public IEnumerator ChangeTextForSeconds(string newtext, float secs) {
+        IsChangingText = true;
+        Debug.Log("Changing text to: " + newtext);
+        string oldtext = text.text;
+        Debug.Log("Saving old text: " + oldtext);
         text.text = newtext;
         yield return new WaitForSeconds(secs);
-        text = oldtext;
+        Debug.Log("Restoring old text: " + oldtext);
+        text.text = oldtext;
+        IsChangingText = false;
+        yield break;
+        // somehow it's not working here but solve the problem in playerchat instead
     }
 
 }
--- Assets/UsernameDisPlay.cs
+++ Assets/UsernameDisPlay.cs
@@ -7,13 +7,15 @@ using TMPro;
 public class UsernameDisPlay : MonoBehaviour
 {
     [SerializeField] PhotonView playerPV;
-    public TMP_Text text;
+    // public TMP_Text text;
+    [SerializeField] TMP_Text text;
 
     void Start()
     {
-        playerPV = GetComponent<PhotonView>();
-        text = GetComponentsInChildren<TMP_Text>()[0];
-        Debug.Log(text);
-        text.text = "username";
+        // playerPV = GetComponent<PhotonView>();
+        // text = GetComponentsInChildren<TMP_Text>()[0];
+        // Debug.Log(text);
+        // text.text = "username";
+        text.text = playerPV.Owner.NickName;
     }
 }
