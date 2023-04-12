333577042a03739703ad534df667c7be835af752
hailey liney <hh20889@bristol.ac.uk>
2023-04-12 18:17:40 +0100

    collider i think

--- Assets/Script/FireCollision.cs
+++ Assets/Script/FireCollision.cs
@@ -11,10 +11,12 @@ public class Fire : MonoBehaviour {
     void Update() {}
 
     void OnCollisionEnter(Collision collision) {
+        Debug.Log("DID YOU PUT YOUR NAME IN DA GOBLET OF FIYAH HARREH, he asked calmly");
+
         GameObject go = collision.gameObject;
         Vector3 direction = collision.relativeVelocity.normalized;
         float magnitude = collision.relativeVelocity.magnitude; 
 
         go.GetComponent<ImpactReceiver>().AddImpact(-direction, magnitude+5.0f); // knocks back foxes
     }
-}
+}   
