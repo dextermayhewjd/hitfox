97ac379ff8025b47bcb55eda101498ed52b143ee
hailey liney <hh20889@bristol.ac.uk>
2023-04-14 14:17:47 +0100

    uhhuH

--- Assets/Script/FireCollision.cs
+++ Assets/Script/FireCollision.cs
@@ -6,17 +6,17 @@ using UnityEngine.AI;
 using System;
 using System.Linq;
 
-public class FireCollision : MonoBehaviour {
+public class FireCollision : OnTrigger {
     void Start() {}
     void Update() {}
 
-    void OnCollisionEnter(Collision collision) {
-        Debug.Log("DID YOU PUT YOUR NAME IN DA GOBLET OF FIYAH HARREH, he asked calmly");
+    // void OnCollisionEnter(Collision collision) {
+    //     Debug.Log("DID YOU PUT YOUR NAME IN DA GOBLET OF FIYAH HARREH, he asked calmly");
 
-        GameObject go = collision.gameObject;
-        Vector3 direction = collision.relativeVelocity.normalized;
-        float magnitude = collision.relativeVelocity.magnitude; 
+    //     GameObject go = collision.gameObject;
+    //     Vector3 direction = collision.relativeVelocity.normalized;
+    //     float magnitude = collision.relativeVelocity.magnitude; 
 
-        go.GetComponent<ImpactReceiver>().AddImpact(-direction, magnitude+5.0f); // knocks back foxes
-    }
+    //     go.GetComponent<ImpactReceiver>().AddImpact(-direction, magnitude+5.0f); // knocks back foxes
+    // }
 }   
