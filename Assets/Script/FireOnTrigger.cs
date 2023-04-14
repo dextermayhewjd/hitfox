c634d97bf623cbbe914e517bc73a2d531cff5890
hailey liney <hh20889@bristol.ac.uk>
2023-04-14 16:57:45 +0100

    fireontrigger.cs

---
+++ Assets/Script/FireOnTrigger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class FireOnTrigger : MonoBehaviourPun
+{
+    public List<Collider> colliders;
+
+    public void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Debug.Log("Player touching");
+            if (!colliders.Contains(col)) colliders.Add(col);
+        }
+    }
+ 
+    public void OnTriggerExit(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Debug.Log("Player left");
+            colliders.Remove(col);
+        }
+    }
+}
