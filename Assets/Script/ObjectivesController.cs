312fc479a3e718ace4181e3abe7c8d42f317e152
Elan Virtucio <ebv2905@gmail.com>
2023-04-22 17:37:12 +0100

    Objectives controller to control adding objectives

---
+++ Assets/Script/ObjectivesController.cs
@@ -0,0 +1,225 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class ObjectivesController : MonoBehaviour
+{
+    [System.Serializable]
+    public class Objective
+    {
+        public string id;
+        public GameObject[] objectsToSpawn;
+        public SpawnLocation[] spawnLocations; 
+
+        private float startTime;
+        private bool active;
+        private List<GameObject> spawnedObjects;
+
+        public Objective(float startTime, List<GameObject> spawnedObjects)
+        {
+            this.active = true;
+            this.startTime = startTime;
+            this.spawnedObjects = spawnedObjects;
+        }
+
+        public void AddSpawnedObject(GameObject newObject)
+        {
+            spawnedObjects.Add(newObject);
+        }
+
+        public int NumbActiveObjects()
+        {
+            int i = 0;
+            foreach (var go in spawnedObjects)
+            {
+                if (go != null)
+                {
+                    i++;
+                }
+            }
+
+            return i;
+        }
+    }
+
+    [Header("Objectives")]
+    [SerializeField] public List<Objective> objectivesList;
+
+    [SerializeField] private GameObject[] trashObjects;
+
+    private Dictionary<string, Objective> objectives;
+    private List<Objective> activeObjectives;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectives = new Dictionary<string, Objective>();
+
+        foreach (var objective in objectivesList)
+        {
+            this.objectives[objective.id] = objective;
+        }
+
+        activeObjectives = new List<Objective>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            // SpawnFire();
+            SpawnTrash(1);
+        }
+        
+        for (int i = 0; i < activeObjectives.Count; i++)
+        {
+            Objective objective = activeObjectives[i];
+            if (objective.NumbActiveObjects() == 0)
+            {
+                activeObjectives.RemoveAt(i);
+                Debug.Log(objective.id + " Objective at Index " + i + " Completed");
+                break;
+            }
+        }
+    }
+
+    public void SpawnTrash(int numTrash = 5, string location = "")
+    {
+        Objective trashObjective;
+        
+        if (!objectives.TryGetValue("Trash", out trashObjective))
+        {
+            Debug.Log("Trash Objective not Set");
+            return;
+        }
+
+        GameObject[] objectsToSpawn = trashObjective.objectsToSpawn;
+        if (objectsToSpawn.Length == 0)
+        {
+            Debug.Log("No Trash Objects to spawn");
+            return;
+        }
+
+        foreach (var go in objectsToSpawn)
+        {
+            if (!IsTrashObject(go.name))
+            {
+                Debug.Log("A non trash object found in objects to spawn");
+                return;
+            }
+        }
+
+        int numSpawnLocations = trashObjective.spawnLocations.Length;
+
+        if (numSpawnLocations == 0)
+        {
+            Debug.Log("Trash spawn location not set");
+            return;
+        }
+
+        int spawnLocationIndex = Random.Range(0, numSpawnLocations);
+        SpawnLocation spawnLocationArea = trashObjective.spawnLocations[spawnLocationIndex];
+
+        if (location != "")
+        {
+            foreach (var loc in trashObjective.spawnLocations)
+            {
+                if (loc.name == location)
+                {
+                    spawnLocationArea = loc;
+                }
+            }
+        }
+
+        List<GameObject> spawnedObjects = new List<GameObject>();
+
+        for (int i = 0; i < numTrash; i++)
+        {
+            Vector3 spawnLocation = spawnLocationArea.GetRandomPoint();
+            GameObject objectToSpawn = objectsToSpawn[Random.Range(0, objectsToSpawn.Length)];
+            GameObject spawnedObject = PhotonNetwork.Instantiate(objectToSpawn.name, spawnLocation, Quaternion.identity);
+            spawnedObjects.Add(spawnedObject);
+            // Debug.Log(objectToSpawn.name + " Spawned At " + spawnLocation);
+        }
+
+        activeObjectives.Add(new Objective(Time.time, spawnedObjects));
+
+        // Add Marker at center of spawnLocationArea and add to quest.
+    }
+
+    public void SpawnFire(string location = "")
+    {
+        Objective fireObjective;
+
+        if (!objectives.TryGetValue("Fire", out fireObjective))
+        {
+            Debug.Log("Fire Objective not Set");
+            return;
+        }
+
+        GameObject objectToSpawn = fireObjective.objectsToSpawn[0];
+        if (objectToSpawn == null)
+        {
+            Debug.Log("Fire Object not set");
+            return;
+        }
+
+        if (objectToSpawn.name != "Fire")
+        {
+            Debug.Log("Object is not a fire object");
+            return;
+        }
+
+        int numSpawnLocations = fireObjective.spawnLocations.Length;
+
+        if (numSpawnLocations == 0)
+        {
+            Debug.Log("Fire spawn location not set");
+            return;
+        }
+
+        int spawnLocationIndex = Random.Range(0, numSpawnLocations);
+        SpawnLocation spawnLocationArea = fireObjective.spawnLocations[spawnLocationIndex];
+
+        if (location != "")
+        {
+            foreach (var loc in fireObjective.spawnLocations)
+            {
+                if (loc.name == location)
+                {
+                    spawnLocationArea = loc;
+                }
+            }
+        }
+
+        Vector3 spawnLocation = spawnLocationArea.GetRandomPoint();
+        // Vector3 spawnLocation = spawnLocationArea.centre;
+
+        List<GameObject> spawnedObjects = new List<GameObject>();
+
+        GameObject spawnedObject = PhotonNetwork.Instantiate(objectToSpawn.name, spawnLocation, Quaternion.identity);
+
+        spawnedObjects.Add(spawnedObject);
+
+        activeObjectives.Add(new Objective(Time.time, spawnedObjects));
+
+        // Debug.Log("Fire Spawned At " + spawnLocation);
+
+        // Add Marker at center of spawnLocationArea and add to quest.
+    }
+
+    private bool IsTrashObject(string name)
+    {
+        foreach (var go in trashObjects)
+        {
+            if (go.name == name)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
--- Assets/Script/SpawnLocation.cs
+++ Assets/Script/SpawnLocation.cs
@@ -4,14 +4,35 @@ using UnityEngine;
 
 public class SpawnLocation : MonoBehaviour
 {
+    public string name;
+
+    public Vector3 centre;
     public Vector3 minPoint;
     public Vector3 maxPoint;
+    public Vector3 extents;
+    public Vector3 size;
 
     void Awake()
     {
         MeshRenderer renderer = GetComponent<MeshRenderer>();
+        centre = renderer.bounds.center;
         minPoint = renderer.bounds.min;
         maxPoint = renderer.bounds.max;
+        extents = renderer.bounds.extents;
+        size = renderer.bounds.size;
         renderer.enabled = false;
+    }
+
+    // Find way to work with rotation of object whilst ignoring scale factor.
+    // Does not really work as intended when object is rotated.
+    public Vector3 GetRandomPoint()
+    {
+        Vector3 point = new Vector3(
+            Random.Range( minPoint.x, maxPoint.x ),
+            Random.Range( minPoint.y, maxPoint.y ),
+            Random.Range( minPoint.z, maxPoint.z )
+        );
+ 
+        return point;
     }
 }
